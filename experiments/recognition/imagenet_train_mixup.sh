9be6aae69ee28887ca83d71a4fffab7084e9c992
whwu <wh.wu@siat.ac.cn>
2020-04-23 19:01:38 +0800

    update mixup script

---
+++ experiments/recognition/imagenet_train_mixup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PYTHON=${PYTHON:-"python"}
+
+$PYTHON -m torch.distributed.launch --nproc_per_node=$2 train_dist_mmcv.py --model $1 \
+--checkname nomixup  --no-bn-wd --last-gamma  --rand-aug  --mixup 0.2  --epochs 270 ${@:7}
+
+# without
+# --rand-aug
+# --mixup 0.2
+# --epochs 270
